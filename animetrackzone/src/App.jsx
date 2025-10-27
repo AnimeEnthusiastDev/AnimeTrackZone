c9f75c9dbc545c481a3676f09fce0090d9fbfed1
unknown <dheenarajesh.g2611@gmail.com>
2025-10-27 12:30:22 +0530

    Initial commit - AnimeTrackZone setup

---
+++ animetrackzone/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to AnimeTrackZone</h1>
+        <button>Like</button>
+      </div>
+    </>
+  )
+}
+
+export default App
